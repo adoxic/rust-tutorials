efa6bd9e89ed2fa3392c6d162380df877edb1af8
adoxic <griffinmaeve@gmail.com>
2021-02-06 12:07:31 -0800

    fuctional mini refactor

--- io_tutorial/src/main.rs
+++ io_tutorial/src/main.rs
@@ -4,14 +4,12 @@ use std::process;
 use io_tutorial::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
+    println!("Searching for \"{}\" in:", config.query);
     println!("{:?}", config.filename);
 
     if let Err(e) = io_tutorial::run(config) {
