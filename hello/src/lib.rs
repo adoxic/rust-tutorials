ca1e76f5942a87f348931fa92b53a1e6d16d5ca3
adoxic <griffinmaeve@gmail.com>
2021-02-12 14:23:25 -0800

    set up threads and workers

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,12 +1,45 @@
-pub struct ThreadPool;
+use std::thread;
+use std::sync::mpsc;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Job;
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    /// "new" function will panic if # of workers in the pool is 0
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool { workers, sender }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,    
     {
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
