a82495064872c986e00f9e11dedefb335b5bc196
adoxic <griffinmaeve@gmail.com>
2021-02-14 11:54:12 -0800

    finished rust tut

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -1,7 +1,7 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
-use std::fs;
 use std::thread;
 use std::time::Duration;
 
@@ -28,7 +28,10 @@ fn handle_connection(mut stream: TcpStream) {
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\nContent-Length; {}\r\n\r\n", "hello.html")
+        (
+            "HTTP/1.1 200 OK\r\nContent-Length; {}\r\n\r\n",
+            "hello.html",
+        )
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,14 +1,19 @@
-use std::thread;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -30,16 +35,24 @@ impl ThreadPool {
     }
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce() + Send + 'static,    
+        F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("shutting down all workers");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -56,18 +69,27 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job; executing.", id);
-            
-            job();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing", id);
+
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+
+                    break;
+                }
+            }
         });
 
-        Worker { 
-            id, 
-            thread: Some(thread), 
+        Worker {
+            id,
+            thread: Some(thread),
         }
     }
 }
